bd71908dc18bb2f56a42a9d1bb3362633cd664f7
Lunahri <jeremy17games@hotmail.com>
2020-06-24 20:53:23 -0300

    Problema 2 Recursividad

---
+++ 2.C++/TPN3_Funciones/Problema_2_Recursividad.cpp
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+int producto(int a, int b);
+
+main()
+{
+	int a,b;
+	printf("\nIngrese dos numeros enteros mayores a 0.\n");
+	
+	printf("\nIngrese el primer numero: ");
+	scanf("%d",&a);
+	printf("\nIngrese el segundo numero: ");
+	scanf("%d",&b);
+
+	printf("\nDivicion: %d",divicion(n1,n2));
+	
+	printf("\n\n");
+	system("pause");
+}
+
+int divicion(int a, int b)
+{
+	if (a>b)
+	{
+		return
+	}
+}
