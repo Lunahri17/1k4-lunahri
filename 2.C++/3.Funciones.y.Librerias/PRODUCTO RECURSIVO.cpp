b1ff5c6b46577c1b8fb83079adb069b02b2e1530
Lunahri <jeremy17games@hotmail.com>
2020-06-24 20:45:31 -0300

    Corregido para función con tipo

--- 2.C++/3.Funciones.y.Librerias/PRODUCTO RECURSIVO.cpp
+++ 2.C++/3.Funciones.y.Librerias/PRODUCTO RECURSIVO.cpp
@@ -1,38 +1,34 @@
-/*Desarrolla una función recursiva que permita obtener el producto de dos números (a y b) enteros
-positivos mayor a cero. Aplicando el concepto de  “sumar el número a la cantidad de veces que indica
-el numero b”.  */
- 
 #include <stdio.h>
 #include <stdlib.h>
 
-void Producto(int a, int b, int &pro);
+int producto(int a, int b);
 
 
 main()
 {
-	int a,b,res;
+	int a,b;
 	printf("\nIngrese dos numeros enteros mayores a 0.\n");
+	
 	printf("\nIngrese el primer numero: ");
-	scanf("%d", &a);
+	scanf("%d",&a);
 	printf("\nIngrese el segundo numero: ");
-	scanf("%d", &b);
-	Producto(a,b,res);
-	printf("\nProducto: %d",res);
+	scanf("%d",&b);
+
+	printf("\nProducto: %d",producto(a,b));
+	
 	printf("\n\n");
 	system("pause");
 }
 
-void Producto(int a, int b, int &pro)
+int producto(int a, int b)
 {
-	int res2;
 	if (b==1)
 	{
-		pro=a;
+		return a;
 	}
 	else
 	{
-		b=b-1;
-		pro=(a+Producto(a,b,res2));
+		return (a+producto(a,b-1));
 	}	
 }
 
