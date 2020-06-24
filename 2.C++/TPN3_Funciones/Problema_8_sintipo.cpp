2d74c117d8e9a1192d2488e1cf2a47fd1312dd13
Lunahri <jeremy17games@hotmail.com>
2020-06-24 18:49:30 -0300

    Problema 8 sin probar

---
+++ 2.C++/TPN3_Funciones/Problema_8_sintipo.cpp
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+main()
+{
+	system("cls");
+
+	int x;
+	float valor=0;
+
+	printf("\nIngrese el tiempo de la llamada: ");
+	scanf("%d",&x);
+
+	if (x==0)
+	{
+		printf("\nNo hubo consumo.");
+	}
+	else
+	{
+		costo(x,valor);
+
+		printf("\nSu llamada costo: %.2f Pesos.",valor);
+	}
+	
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
+
+
+void valor(int x1,float &valor1)
+{
+	int var1;
+	
+	if(x<4)
+	{
+		valor1=0.70;
+	}
+	else
+	{
+		var1=x1-3;
+		valor1=0.7+(var1*0.25);
+	}
+	
+}
