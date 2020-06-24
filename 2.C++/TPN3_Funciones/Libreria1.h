fada31d2a600c3961434c1768f8f3e2f291bca6f
Lunahri <jeremy17games@hotmail.com>
2020-06-24 19:33:53 -0300

    Funcion 2

--- 2.C++/TPN3_Funciones/Libreria1.h
+++ 2.C++/TPN3_Funciones/Libreria1.h
@@ -16,3 +16,18 @@ int mayor(int x1,int x2,int x3)
 }
 
 //Funcion (2): Retorna el menor de 3 numeros.
+int menor(int x1,int x2,int x3)
+{
+    if (x1<x2 and x1<x3)
+    {
+        return x1;
+    }
+    if(x2<x1 and x2<x3)
+    {
+        return x2;
+    }
+    else
+    {
+        return x3;
+    } 
+}
