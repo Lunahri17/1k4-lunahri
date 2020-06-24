63ff14382a22a71833f18c71f988cba0111caf4e
Lunahri <jeremy17games@hotmail.com>
2020-06-24 18:12:54 -0300

    Problema 7 solo porcentaje

---
+++ 2.C++/TPN3_Funciones/Problema_7_contipo.cpp
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+main()
+{
+    int x,contpar=0,contimpar=0;
+
+    printf("Ingrese un numero (0 para cancelar): ");
+    scanf("%d",&x);
+
+    while (x!=0)
+    {  
+        if (x%2==0)
+        {
+            contpar++;
+        }
+        else
+        {
+            contimpar++;
+        }
+        
+        system("cls");
+        printf("\nIngrese otro numero, o un 0 para cancelar: ");
+        scanf("%d",&x);
+    }
+    
+    printf("\nPorcentaje de numeros impares: %.2f",porcentaje(contpar,contimpar));
+
+ 	printf("\n\n");
+	system("pause");
+}
+
+float porcentaje(int numpar,int numimp)
+{
+    float por;
+
+    por=(float)numimp*100/(numpar+numimp);
+    return por;
+}
