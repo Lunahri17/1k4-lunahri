27f016e72107d3ab17c8d213c08348e7676f4525
Lunahri <jeremy17games@hotmail.com>
2020-06-24 19:31:11 -0300

    Librería función 1

---
+++ 2.C++/TPN3_Funciones/Libreria1.h
@@ -0,0 +1,18 @@
+//Función (1): Retorna el mayor de 3 numeros.
+int mayor(int x1,int x2,int x3)
+{
+    if (x1>x2 and x1>x3)
+    {
+        return x1;
+    }
+    if(x2>x1 and x2>x3)
+    {
+        return x2;
+    }
+    else
+    {
+        return x3;
+    } 
+}
+
+//Funcion (2): Retorna el menor de 3 numeros.
