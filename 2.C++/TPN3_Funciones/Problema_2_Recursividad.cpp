bc7b6587342f04a18d9f79e58dc2beeb750beec1
Lunahri <jeremy17games@hotmail.com>
2020-06-24 21:18:56 -0300

    Problema 2 Recursividad

--- 2.C++/TPN3_Funciones/Problema_2_Recursividad.cpp
+++ 2.C++/TPN3_Funciones/Problema_2_Recursividad.cpp
@@ -2,28 +2,57 @@
 #include<stdlib.h>
 #include<math.h>
 
-int producto(int a, int b);
+int division(int a, int b);
 
 main()
 {
-	int a,b;
-	printf("\nIngrese dos numeros enteros mayores a 0.\n");
-	
-	printf("\nIngrese el primer numero: ");
-	scanf("%d",&a);
-	printf("\nIngrese el segundo numero: ");
-	scanf("%d",&b);
+    system("cls");
 
-	printf("\nDivicion: %d",divicion(n1,n2));
+	int n1,n2;
+	
+	do
+    {
+        printf("\nIngrese el dividendo (Debe ser el mayor): ");
+	    scanf("%d",&n1);
+
+        if (n1<0)
+        {
+            printf("\nEl numero ingresado debe ser mayor que cero. Intente Nuevamente.\n");
+            system("pause");
+        }
+    } while (n1<0);
+    
+	do
+    {
+        printf("\nIngrese el divisor. Debe ser mayor 0 y menor que el dividendo %d: ",n1);
+	    scanf("%d",&n2);
+        
+        if (n2>n1 or n2==0)
+        {
+            printf("\nEl numero ingresado debe ser mayor que cero y menor que el dividendo %d. Intente Nuevamente.\n",n1);
+            system("pause");
+        }
+    }while (n2>n1 or n2==0);
+    
+
+	printf("\nResultado de la division: %d",division(n1,n2));
 	
 	printf("\n\n");
 	system("pause");
 }
 
-int divicion(int a, int b)
+int division(int a, int b)
 {
-	if (a>b)
-	{
-		return
-	}
+	int div;
+
+    if ((a-b)<0)
+    {
+        div=0;
+    }
+    else
+    {
+        div=1+division(a-b,b);
+    }
+    return div;
+    
 }
