2b6267993670d9aef12681ec8691bee44481a5a4
Lunahri <jeremy17games@hotmail.com>
2020-06-29 12:08:32 -0300

    Funcion mostrar

--- 2.C++/5.Datos.Estructurados/2.Trabajo Clase Teórica - Vectores.Paralelos(Ordenamiento-Búsqueda)/Problema_1.cpp
+++ 2.C++/5.Datos.Estructurados/2.Trabajo Clase Teórica - Vectores.Paralelos(Ordenamiento-Búsqueda)/Problema_1.cpp
@@ -2,7 +2,9 @@
 #include<stdlib.h>
 #include<math.h>
 
-
+void leer(int n1,float pro1[50],int leg1[50]);
+void orden(int n1,float pro1[50],int leg1[50]);
+void mostrar(int n1,float pro1[50],int leg1[50]);
 
 main()
 {
@@ -14,8 +16,10 @@ main()
 	
 	leer(n,pro,leg);
 	orden(n,pro,leg);
-
-
+	mostrar(n,pro,leg);
+	
+	printf("\n\tVectores ordenados: ");
+	
  	printf("\n\n");
 	system("pause");
 }
@@ -24,10 +28,10 @@ void leer(int n1,float pro1[50],int leg1[50])
 {
 	int i;
 	
-	for(i=0;i<n;i++)
+	for(i=0;i<n1;i++)
 	{
 		printf("\n pro[%d]= ",i);
-		scanf("%f",&por1);
+		scanf("%f",&pro1);
 		
 		printf("\n leg[%d]= ",i);
 		scanf("%f",&leg1);
@@ -36,13 +40,13 @@ void leer(int n1,float pro1[50],int leg1[50])
 
 void ordenar (int n1,float pro1[50],int leg1[50])
 {
-	int i,aux;
+	int i,aux,b;
 	
 	do
 	{
 		b=0;
 		//vector ordenado
-		for(i=0;i<n-1;i++)
+		for(i=0;i<n1-1;i++)
 		{
 			if(pro1[i]>pro1[i])
 			{
@@ -53,12 +57,25 @@ void ordenar (int n1,float pro1[50],int leg1[50])
 				aux=leg1[i];
 				leg1[i]=leg1[i+1];
 				leg1[i+1]=aux;
+				
+				b=1;
+				//Vector ordenado
 			}
 		}
 	}
+	while(b==1);
 }
 
-
+void mostrar(int n1,float pro1[50],int leg1[50])
+{
+	int i;
+	
+	for(i=0;i<n1;i++)
+	{
+		printf("\n pro[%d]= %.2f",i,pro1[i]);
+		printf("\n leg[%d]= %.2f",i,leg1[i]);
+	}
+}
 
 
 
