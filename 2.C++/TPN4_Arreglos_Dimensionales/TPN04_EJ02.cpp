3440f57fe78e163fdc29da99b7fbd6a7074bde35
Lunahri <jeremy17games@hotmail.com>
2020-07-08 16:54:04 -0300

    Función CargarNotas

--- 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ02.cpp
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ02.cpp
@@ -6,11 +6,32 @@
 
 main()
 {
+	int n;
+	float notas[200];
 
+	printf(" Ingrese la cantidad de notas:");
+	scanf("%d",&n);
+
+	CargarNotas(n,notas);
     
 	
 	system("pause");
     printf("\n\tGracias por utilizar el programa.");
  	printf("\n\n");
 	system("pause");
+}
+
+void CargarNotas(int n1, float notas1[200])
+{
+	for (int i=1;i<=n1;i++)
+	{
+		printf("\n Ingrese la nota %d: ",i);
+		scanf("%f",&notas1[i]);
+
+		if (notas1[i]<1 or notas1[i]>10)
+		{
+			printf("\nIngreso una nota erronea. Vuelva a ingresarla.");
+			i--;
+		}
+	}
 }
