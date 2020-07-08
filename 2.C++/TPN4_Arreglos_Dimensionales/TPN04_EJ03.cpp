628da0a813abadf3758cabe0aaf3c24a11043b2f
Lunahri17 <lunahri@outlook.com>
2020-07-08 19:21:26 -0300

    Ej3 listo para codigo

---
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ03.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+
+
+main()
+{
+
+    
+	
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
