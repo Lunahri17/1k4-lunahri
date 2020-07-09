abb6eed589df3e19c61fc3ff8ffebabf3e54c5f5
Lunahri <jeremy17games@hotmail.com>
2020-07-09 15:14:40 -0300

    Función CargarValores

--- 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ03.cpp
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ03.cpp
@@ -2,15 +2,37 @@
 #include<stdlib.h>
 #include<math.h>
 
-
+int CargarValores(int vector1[100]);
 
 main()
 {
+	int n,vector[100];
 
+	n=CargarValores(vector);
     
 	
 	system("pause");
     printf("\n\tGracias por utilizar el programa.");
  	printf("\n\n");
 	system("pause");
+}
+
+int CargarValores(int vector1[100])
+{
+	int n,i=0;
+
+	do
+	{
+		printf("\n Ingrese el valor, o ingrese 0 para terminar: ");
+		scanf("%d",&n);
+		
+		if (n!=0)
+		{
+			vector1[i]=n;
+			i++;	
+		}
+	}
+	while (n!=0);
+	
+	return i;
 }
