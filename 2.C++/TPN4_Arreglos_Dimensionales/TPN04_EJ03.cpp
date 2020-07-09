d1bee7373e5509df2d22d715a0c344bd4cffba38
Lunahri <jeremy17games@hotmail.com>
2020-07-09 15:39:27 -0300

    Función Buscar

--- 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ03.cpp
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ03.cpp
@@ -6,11 +6,19 @@ int CargarValores(int vector1[100]);
 
 main()
 {
-	int n,vector[100];
+	int n,vector[100],ValorBuscar,Cantidad=0;
+	bool esta=0;
 
 	n=CargarValores(vector);
     
+	printf("\n Ingrese el valor a buscar: ");
+	scanf("%d",&ValorBuscar);
 	
+	Buscar(n,vector,ValorBuscar,Cantidad);
+
+	printf("\n El valor %d esta en el vector, y se repitio %d.",ValorBuscar,Cantidad);
+	
+
 	system("pause");
     printf("\n\tGracias por utilizar el programa.");
  	printf("\n\n");
@@ -35,4 +43,28 @@ int CargarValores(int vector1[100])
 	while (n!=0);
 	
 	return i;
+}
+
+bool Buscar(int n1,int vector1[100],int valor,int &cant)
+{
+	cant=0
+
+	for (int i=0;i<n1;i++)
+	{
+		if (valor==vector1[i])
+		{
+			cant++;
+		}
+	}
+	
+	if (cant>0)
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+	
+	
 }
