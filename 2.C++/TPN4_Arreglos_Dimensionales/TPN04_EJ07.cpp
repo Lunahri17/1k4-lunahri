a0da884fc44d606e3b8ca0d14b527b9110fa0e64
Lunahri <jeremy17games@hotmail.com>
2020-07-10 20:03:54 -0300

    Ejercicio 7 reddy for code xd

---
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ07.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+void end();
+
+
+main()
+{
+
+    
+	
+	end();
+}
+
+
+
+void end()
+{
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
