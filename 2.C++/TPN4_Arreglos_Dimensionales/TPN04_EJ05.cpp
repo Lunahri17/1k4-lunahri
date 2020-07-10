fda76db462a316c59bad85b58a2bd16ee6db87e4
Lunahri <jeremy17games@hotmail.com>
2020-07-10 15:49:14 -0300

    Función CargarDatosRand

--- 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ05.cpp
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ05.cpp
@@ -1,18 +1,30 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<ctime>
 
 void end();
 
 
 main()
 {
+	int vector[100];
 
+	CargarDatosRand(vector);
     
 	
 	end();
 }
 
+void CargarDatosRand(int vector1[100])
+{
+    srand(time(0));
+
+    for (int i = 0; i < 100; i++)
+    {
+        vector1[i]=rand();
+    }
+}
 
 
 void end()
