1184de1515b10108e1bcb38fd9156285e81c92ef
Lunahri <jeremy17games@hotmail.com>
2020-07-10 16:01:40 -0300

    Función Promedios

--- 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ05.cpp
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ05.cpp
@@ -5,14 +5,15 @@
 
 void end();
 void CargarDatosRand(int vector1[100]);
-
+void Promedios(int vector1[100],float &PromedioPar,float &PromedioImpar);
 
 main()
 {
 	int vector[100];
+	float PromedioPar,PromedioImpar;
 
 	CargarDatosRand(vector);
-    
+    Promedios(vector,PromedioPar,PromedioImpar);
 	
 	end();
 }
@@ -25,6 +26,28 @@ void CargarDatosRand(int vector1[100])
     {
         vector1[i]=rand();
     }
+}
+
+void Promedios(int vector1[100],float &PromedioPar,float &PromedioImpar)
+{
+	int AcumuladorPar,ContadorPar=0,AcumuladorImpar,ContadorImpar=0;
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (vector1[i]%2==0)
+		{
+			AcumuladorPar=AcumuladorPar+vector1[i];
+			ContadorPar++;
+		}
+		else
+		{
+			AcumuladorImpar=AcumuladorImpar+vector1[i];
+			ContadorImpar++;
+		}
+	}
+
+	PromedioPar=(float)AcumuladorPar/ContadorPar;
+	PromedioImpar=(float)AcumuladorImpar/ContadorImpar;
 }
 
 
