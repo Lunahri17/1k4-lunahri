48fe452f429a68d387f43e3d550b7f5dbef1841a
Lunahri <jeremy17games@hotmail.com>
2020-07-10 20:09:29 -0300

    Función CargarVector y Mostrar

--- 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ07.cpp
+++ 2.C++/TPN4_Arreglos_Dimensionales/TPN04_EJ07.cpp
@@ -3,16 +3,42 @@
 #include<math.h>
 
 void end();
+void CargarVector(int vector1[10]);
+void Mostrar(int vector1[10]);
 
 
 main()
 {
+    int vector[10],;
 
+    CargarVector(vector);
+
+    system("cls");
+
+    printf("\n Vector ingresado es: ");
+    Mostrar(vector);
     
 	
 	end();
 }
 
+void CargarVector(int vector1[10])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\n Ingrese el valor %d del arreglo: ",i+1);
+        scanf("%d",&vector1[i]);
+    }
+}
+
+void Mostrar(int vector1[10])
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\t%d",vector1[i]);
+    }
+    
+}
 
 
 void end()
