a23582760c6120fc6137fda9ab09c0dae1f0acf0
Lunahri <jeremy17games@hotmail.com>
2020-08-25 18:27:15 -0300

    Corrección for con j++ y tercer punto first parte

--- 2.C++/5.Datos.Estructurados/3.Trabajo Clase Teorica - Arreglo Bidimensional (Matrices)/Punto.1.cpp
+++ 2.C++/5.Datos.Estructurados/3.Trabajo Clase Teorica - Arreglo Bidimensional (Matrices)/Punto.1.cpp
@@ -28,7 +28,7 @@ main()
 		//inicializar por filas:
 		s=0;
 
-		for (int j = 0; j < n; i++)
+		for (int j = 0; j < n; j++)
 		{
 			s+=a[i][j];
 		}
@@ -41,6 +41,30 @@ main()
 	}
 
 	printf("\nLa sucursal con mayor valor de ventas fue la %d.",suc);
+
+	//Tercer punto:
+	mayc=0;
+	dep=0;
+	
+	for (j = 0; j < m; j++) //aqui si importa que primero vaya el cilco de i, porque tenemos que saber por sucursal
+	{
+		//inicializar por filas:
+		s=0;
+
+		for (i = 0; i < n; i++)
+		{
+			s+=a[i][j];
+		}
+		
+		if (s>mayc)
+		{
+			mayc=s;
+			dep=j;
+		}	
+	}
+
+	printf("\nLa sucursal con mayor valor de ventas fue la %d.",suc);
+
 
 	//Cuarto Punto:
 	mt=total(m,n,a);
@@ -57,7 +81,7 @@ void leer(int m1,int n1,float a1[50][50])
 	//primero ciclo de i --> por filas
 	for (i = 0; i < m1; i++)
 	{
-		for (j = 0; j < n1; i++)
+		for (j = 0; j < n1; j++)
 		{
 			printf("a[%d][%d]= ",i,j);
 			scanf("%f",&a1[i][j]);
@@ -75,7 +99,7 @@ float total(int m1,int n1,float a1[50][50])
 
 	for (i = 0; i < m1; i++)
 	{
-		for (j = 0; j < n1; i++)
+		for (j = 0; j < n1; j++)
 		{
 			s+=a1[i][j];
 		}	
