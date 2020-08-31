4fe41918f5f5223e2f3dc038741fe9f38c224104
Lunahri <jeremy17games@hotmail.com>
2020-08-31 17:35:39 -0300

    Función MostrarMatriz.

--- 2.C++/5.Datos.Estructurados/4.Trabajo Clase Teórica - Arreglo Bidimensional/ArregloBidimensional.cpp
+++ 2.C++/5.Datos.Estructurados/4.Trabajo Clase Teórica - Arreglo Bidimensional/ArregloBidimensional.cpp
@@ -5,7 +5,7 @@
 //Protipos de funciones:
 void end();
 void CargarMatriz(int m,int Matriz[100][100]);
-
+void MostrarMatriz(int m,int Matriz[100][100]);
 
 //Variables globales:
 
@@ -18,6 +18,7 @@ main()
 	scanf("%d",&m);
 
 	CargarMatriz(m,Matriz);
+	MostrarMatriz(m,Matriz);
 	
 	end();
 }
@@ -30,13 +31,24 @@ void CargarMatriz(int m,int Matriz[100][100])
 		{
 			if (i<=j)
 			{
-				printf("a[%d][%d]= ",i,j);
+				printf("Matriz[%d][%d]= ",i,j);
 				scanf("%d",&Matriz[100][100]);
 			}
 			else
 			{
 				a[i][j]=-a[j][i];
 			}
+		}
+	}
+}
+
+void MostrarMatriz(int m,int Matriz[100][100])
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			printf("Matriz[%d][%d]= %d",i,j,Matriz[i][j]);
 		}
 	}
 }
