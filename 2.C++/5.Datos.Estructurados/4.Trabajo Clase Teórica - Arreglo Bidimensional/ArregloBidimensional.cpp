e7165696d3d98603dce552c0feb57a4939b5f5b1
Lunahri <jeremy17games@hotmail.com>
2020-08-31 17:29:45 -0300

    Carga matriz antisimetrica.

--- 2.C++/5.Datos.Estructurados/4.Trabajo Clase Teórica - Arreglo Bidimensional/ArregloBidimensional.cpp
+++ 2.C++/5.Datos.Estructurados/4.Trabajo Clase Teórica - Arreglo Bidimensional/ArregloBidimensional.cpp
@@ -11,12 +11,34 @@ void end();
 
 main()
 {
-	
+	int m,s,Matriz[100][100];
+
+    printf("\nIngrese la cantidad de filas de la matriz cuadrada: ");
+	scanf("%d",&m);
+
+	CargarVector(m,Matriz);
 	
 	end();
 }
 
-
+void CargarVector(int m,int Matriz[100][100])
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (i<=j)
+			{
+				printf("a[%d][%d]= ",i,j);
+				scanf("%d",&Matriz[100][100]);
+			}
+			else
+			{
+				a[i][j]=-a[j][i];
+			}
+		}
+	}
+}
 
 void end()
 {
