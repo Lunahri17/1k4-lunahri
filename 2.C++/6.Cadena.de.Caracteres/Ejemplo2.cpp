62d17e4d4a3a9240fc5c827ae2b22b56020e904e
Lunahri17 <jeremy17games@hotmail.com>
2020-09-14 16:06:56 -0300

    Archivos clase 14/09/2020

---
+++ 2.C++/6.Cadena.de.Caracteres/Ejemplo2.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+main()
+{
+    char medicamento[30],buscar[30];
+    int pos;
+
+    printf("\n\nIngrese el Medicamento");
+    gets(medicamento);
+
+    printf("\n\nBuscar");
+
+
+
+        //incompleto xd
+}
