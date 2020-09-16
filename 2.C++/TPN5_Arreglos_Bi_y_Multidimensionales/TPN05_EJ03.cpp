ffd0c334116cb9279c959a3a393c2e07f81da121
Lunahri17 <jeremy17games@hotmail.com>
2020-09-16 18:03:50 -0300

    Función CargarMatriz.

--- 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ03.cpp
+++ 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ03.cpp
@@ -1,21 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include<ctime>
 
 //Protipos de funciones:
 void end();
 
 
-//Variables globales:
-
-
 main()
 {
-	
+    srand(time(0));
+
+	int Matriz[100][3],NroTalleres;
+
+    printf("\nIngrese la cantidad de talleres: ");
+    scanf("%d",&NroTalleres);
+
+    CargarMatriz(Matriz,NroTalleres);
 	
 	end();
 }
 
+void CargarMatriz(int Matriz[100][3],int NroTalleres)
+{
+    for (int j = 0; j < NroTalleres; j++)
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Matriz[i][j]=1+rand%1000; //Decidí poner como tope 1000, así lo numero no se hagan muy grandes a la hora de probar el programa.
+        }
+    }
+}
 
 
 void end()
