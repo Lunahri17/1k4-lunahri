56a6a29f5af1913bc65ab728ea36863f8abb0359
Lunahri17 <jeremy17games@hotmail.com>
2020-09-18 20:58:51 -0300

    Es literalmente lo mismo que el 5 sin promedio xd.

--- 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ08.cpp
+++ 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ08.cpp
@@ -4,15 +4,74 @@
 
 //Protipos de funciones:
 void end();
+void CargarArreglo(int Arreglo[50][50][50],int Filas,int Columnas,int Planos);
+void SumaPlanos(int Arreglo[50][50][50],int Filas,int Columnas,int Planos,int Suma[50]);
+void MostrarVector(int Vector[50],int Indice);
 
 main()
 {
-	
+	int Arreglo[50][50][50],Suma[50],Filas,Columnas,Planos;
+
+    printf("\nIngrese la cantidad de filas: ");
+    scanf("%d",&Filas);
+    printf("\nIngrese la cantidad de columnas: ");
+    scanf("%d",&Columnas);
+    printf("\nIngrese la cantidad de planos: ");
+    scanf("%d",&Planos);
+
+    printf("\nCargue el arreglo: ");
+    CargarArreglo(Arreglo,Filas,Columnas,Planos);
+
+    SumaPlanos(Arreglo,Filas,Columnas,Planos,Suma);
+
+    printf("\nVector con la suma de los planos: ");
+    MostrarVector(Suma,Planos);
 	
 	end();
 }
 
+void CargarArreglo(int Arreglo[50][50][50],int Filas,int Columnas,int Planos)
+{
+    for (int k = 0; k < Planos; k++)
+    {
+        for (int j = 0; j < Columnas; j++)
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                printf("\nArreglo[%d][%d][%d]= ",i,j,k);
+                scanf("%d",&Arreglo[i][j][k]);
+            }
+        }
+    }
+}
 
+void SumaPlanos(int Arreglo[50][50][50],int Filas,int Columnas,int Planos,int Suma[50])
+{
+    int Acumulador=0;
+    
+    for (int k = 0; k < Planos; k++)
+    {
+        Acumulador=0;
+
+        for (int j = 0; j < Columnas; j++)
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                Acumulador+=Arreglo[i][j][k];
+            }
+        }
+
+        Suma[k]=Acumulador;
+    }
+}
+
+void MostrarVector(int Vector[50],int Indice)
+{
+    for (int i = 0; i < Indice; i++)
+    {
+        printf("%d ",Vector[i]);
+    }
+}
 
 void end()
 {
