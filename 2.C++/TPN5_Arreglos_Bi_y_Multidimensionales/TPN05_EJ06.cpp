e735b3059001ad1774758f58e20459e95f668f36
Lunahri17 <jeremy17games@hotmail.com>
2020-09-18 18:26:44 -0300

    Función CargarArreglo y MenorValor.

--- 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ06.cpp
+++ 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ06.cpp
@@ -4,16 +4,64 @@
 
 //Protipos de funciones:
 void end();
-
+void CargarArreglo(float Arreglo[50][50][50],int Filas,int Columnas,int Planos);
 
 main()
 {
-	
+	int Filas,Columnas,Planos;
+    float Arreglo[50][50][50],Menor;
+
+    printf("\nIngrese la cantidad de filas: ");
+    scanf("%d",&Filas);
+    printf("\nIngrese la cantidad de columnas: ");
+    scanf("%d",&Columnas);
+    printf("\nIngrese la cantidad de planos: ");
+    scanf("%d",&Planos);
+
+    printf("\nCargue el arreglo: ");
+    CargarArreglo(Arreglo,Filas,Columnas,Planos);
+
+    Menor=MenorValor(Arreglo,Filas,Columnas,Planos);
+    printf("\nEl menor valor encontrado es: %.2f",Menor);
+
+    
 	
 	end();
 }
 
+void CargarArreglo(float Arreglo[50][50][50],int Filas,int Columnas,int Planos)
+{
+    for (int k = 0; k < Planos; k++)
+    {
+        for (int j = 0; j < Columnas; j++)
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                printf("\nArreglo[%d][%d][%d]= ",i,j,k);
+                scanf("%d",&Arreglo[i][j][k]);
+            }
+        }
+    }
+}
 
+float MenorValor(float Arreglo[50][50][50],int Filas,int Columnas,int Planos)
+{
+    float Menor=Arreglo[0][0][0];
+    
+    for (int k = 0; k < Planos; k++)
+    {
+        for (int j = 0; j < Columnas; j++)
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                if (Arreglo[i][j][k]<Menor)
+                {
+                    Menor=Arreglo[i][j][k];
+                }                
+            }
+        }
+    }
+}
 
 void end()
 {
