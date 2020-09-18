d0b3e5631e578956fdbd57998c967ab73c37e952
Lunahri17 <jeremy17games@hotmail.com>
2020-09-18 18:52:25 -0300

    Archivo para punto 7.

---
+++ 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ07.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//Protipos de funciones:
+void end();
+
+main()
+{
+	
+	
+	end();
+}
+
+
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
