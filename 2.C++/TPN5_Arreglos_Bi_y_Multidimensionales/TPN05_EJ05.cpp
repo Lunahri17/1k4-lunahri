6e218d9b1cb005362cb3dfa63e5d2646697af77d
Lunahri17 <jeremy17games@hotmail.com>
2020-09-18 18:17:19 -0300

    Faltaba ;

--- 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ05.cpp
+++ 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ05.cpp
@@ -7,7 +7,7 @@ void end();
 void CargarArreglo(int Arreglo[50][50][50],int Filas,int Columnas,int Planos);
 void SumaPlanos(int Arreglo[50][50][50],int Filas,int Columnas,int Planos,int Suma[50]);
 void MostrarVector(int Vector[50],int Indice);
-float Promedio(int Vector[50],int Indice)
+float Promedio(int Vector[50],int Indice);
 
 main()
 {
