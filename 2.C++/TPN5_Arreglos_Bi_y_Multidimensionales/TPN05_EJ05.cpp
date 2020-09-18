b301df1ddd830285704adb9e876d1efd145e0e7d
Lunahri17 <jeremy17games@hotmail.com>
2020-09-18 16:30:17 -0300

    Función CargarArreglo.

--- 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ05.cpp
+++ 2.C++/TPN5_Arreglos_Bi_y_Multidimensionales/TPN05_EJ05.cpp
@@ -4,12 +4,37 @@
 
 //Protipos de funciones:
 void end();
+void CargarArreglo(int Arreglo[100][100][100],int Filas,int Columnas,int Planos);
 
 main()
 {
-	
-	
+	int Arreglo[100][100][100],Filas,Columnas,Planos;
+
+    printf("\nIngrese la cantidad de filas: ");
+    scanf("%d",&Filas);
+    printf("\nIngrese la cantidad de columnas: ");
+    scanf("%d",&Columnas);
+    printf("\nIngrese la cantidad de planos: ");
+    scanf("%d",&Planos);
+
+    CargarArreglo(Arreglo,Filas,Columnas,Planos);
+
 	end();
+}
+
+void CargarArreglo(int Arreglo[100][100][100],int Filas,int Columnas,int Planos)
+{
+    for (int k = 0; k < Planos; k++)
+    {
+        for (int j = 0; j < Columnas; j++)
+        {
+            for (int i = 0; i < Filas; i++)
+            {
+                printf("\nArreglo[%d][%d][%d]= ",i,j,k);
+                scanf("%d",&Arreglo[i][j][k]);
+            }
+        }
+    }
 }
 
 
