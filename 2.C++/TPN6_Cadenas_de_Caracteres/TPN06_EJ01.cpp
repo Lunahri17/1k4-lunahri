1ec208c59868a5d42852fccba8e408878aa51b0e
Lunahri17 <jeremy17games@hotmail.com>
2020-09-30 18:06:57 -0300

    Problema 1 listo.

--- 2.C++/TPN6_Cadenas_de_Caracteres/TPN06_EJ01.cpp
+++ 2.C++/TPN6_Cadenas_de_Caracteres/TPN06_EJ01.cpp
@@ -8,8 +8,43 @@ void end();
 
 main()
 {
-	
-	
+	char Palabra[] = "final",PalabraMasLarga[50]="a";
+
+    typedef char Cadena[50];
+    Cadena Palabras[50];
+    
+    int i=0;
+    
+    bool stop=false;
+
+    do
+    {
+        printf("Ingrese la palabra %d: ",i);
+        _flushall();
+        gets(Palabras[i]);
+        strlwr(Palabras[i]);
+
+        if (strcmp(Palabras[i],Palabra)!=0)
+        {
+            if (strlen(Palabras[i])>strlen(PalabraMasLarga))
+            {
+                strcpy(PalabraMasLarga,Palabras[i]);
+            }
+
+            i++;
+        }
+        else
+        {
+            stop=true;
+        }
+
+    } while (stop==false);
+    
+	printf("La palabra mas larga ingresada fue: ");
+    puts(PalabraMasLarga);
+
+    printf("Se ingresaron %d palabras",i);
+
 	end();
 }
 
