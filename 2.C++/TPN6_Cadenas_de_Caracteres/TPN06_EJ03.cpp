63c9df1c9a6f6ee93cb218035d74f4dfbf7dd963
Lunahri17 <jeremy17games@hotmail.com>
2020-10-01 17:07:59 -0300

    Solucion don variables globales.

--- 2.C++/TPN6_Cadenas_de_Caracteres/TPN06_EJ03.cpp
+++ 2.C++/TPN6_Cadenas_de_Caracteres/TPN06_EJ03.cpp
@@ -5,30 +5,31 @@
 
 //Protipos de funciones:
 void end();
-void CargarVectores(cadena Asignaturas[30],int Inscriptos[100],int NroDeAsignaturas);
+void CargarVectores(int Inscriptos[100],int NroDeAsignaturas);
+
+//Variables Globales
+typedef char cadena[30];
+cadena Asignaturas[30];
 
 main()
 {
-	typedef cadena[100];
-    cadena Asignaturas[30];
-
-    int NroDeAsignaturas,Inscriptos[100];
+	int NroDeAsignaturas,Inscriptos[100];
 
     printf("\nIngrese la cantidad de asignaturas: ");
     scanf("%d",&NroDeAsignaturas);
 
-    CargarVectores(Asignaturas,Inscriptos,NroDeAsignaturas);
+    CargarVectores(Inscriptos,NroDeAsignaturas);
 	
 	end();
 }
 
-void CargarVectores(cadena Asignaturas[30],int Inscriptos[100],int NroDeAsignaturas)
+void CargarVectores(int Inscriptos[100],int NroDeAsignaturas)
 {
     for (int i = 0; i < NroDeAsignaturas; i++)
     {
         printf("\nIngerese el nombre se la asgnatura %d",i+1);
         _flushall();
-        gets(Asignatura[i]);
+        gets(Asignaturas[i]);
         _flushall();
 
         printf("\nIngrese la cantidad de alumanos inscriptos de la asignatura %s",Asignaturas[i]);
