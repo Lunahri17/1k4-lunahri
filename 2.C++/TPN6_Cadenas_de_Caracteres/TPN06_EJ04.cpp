23c1110d343e508945b4eee0b28a2c64b38e7924
Lunahri17 <jeremy17games@hotmail.com>
2020-10-02 16:30:07 -0300

    Solucionado contabilización de may y men.

--- 2.C++/TPN6_Cadenas_de_Caracteres/TPN06_EJ04.cpp
+++ 2.C++/TPN6_Cadenas_de_Caracteres/TPN06_EJ04.cpp
@@ -18,32 +18,28 @@ main()
 
     do
     {
-        //Busqueda de consonantes Mayusculas y letras maysuculas.
+        //Busqueda de consonantes Mayusculas.
         if ((poema[i]>65 and poema[i]<69) or (poema[i]>69 and poema[i]<73) or (poema[i]>73 and poema[i]<79) or (poema[i]>79 and poema[i]<85) or (poema[i]>85 and poema[i]<91))
         {
             Consonantes++;
-            Mayusculas++;
         }
         
-        //Busqueda de consonantes Minusculas y letras minusculas.
+        //Busqueda de consonantes Minusculas.
         if ((poema[i]>97 and poema[i]<101) or (poema[i]>101 and poema[i]<105) or (poema[i]>105 and poema[i]<111) or (poema[i]>111 and poema[i]<117) or (poema[i]>117 and poema[i]<123))
         {
             Consonantes++;
-            Minusculas++;
         }
 
-        //Busqueda de vocales Mayusculas y letras mayusculas.
+        //Busqueda de vocales Mayusculas.
         if (poema[i]==65 or poema[i]==69 or poema[i]==73 or poema[i]==65 or poema[i]==65)
         {
             Vocales++;
-            Mayusculas++;
         }
         
-        //Busqueda de vocales minusculas y letras minusculas.
+        //Busqueda de vocales minusculas
         if (poema[i]==97 or poema[i]==101 or poema[i]==105 or poema[i]==111 or poema[i]==117)
         {
             Vocales++;
-            Mayusculas++;
         }
         
         //Busqueda de digitos.
@@ -51,7 +47,6 @@ main()
         {
             Digitos++;
         }
-        
 
         //Singos de puntuación.
         if ((poema[i]>57 and poema[i]<64) or (poema[i]>93 and poema[i]<97) or (poema[i]>122 and poema[i]<127) or poema[i]==168 or poema[i]==173 or poema[i]==33 or (poema[i]>38 and poema[i]<47) or (poema[i]>237 and poema[i]<241))
@@ -63,6 +58,18 @@ main()
         if (poema[i]==32)
         {
             Espacios++;
+        }
+
+        //Letras mayusculas.
+        if(poema[i]>64 and poema[i]<91)
+        {
+            Mayusculas++;
+        }
+
+        //Letras minusculas.
+        if(poema[i]>96 and poema[i]<123)
+        {
+            Minusculas++;
         }
 
         i++;
