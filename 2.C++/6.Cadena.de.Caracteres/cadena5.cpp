d0944c0298b1a5a742c4b11d0fe1d3f9f686312c
Lunahri17 <jeremy17games@hotmail.com>
2020-10-02 17:27:32 -0300

    System pause para poder ver el resultado.

--- 2.C++/6.Cadena.de.Caracteres/cadena5.cpp
+++ 2.C++/6.Cadena.de.Caracteres/cadena5.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include<stdlib.h>
 
 main()
 {
@@ -23,7 +24,7 @@ main()
          b=0;
          for (i=0;i<n-1;i++)
          {
-             if(strcmp(nombre[i],nombre[i+1])>0)
+             if(strcmp(nombre[i],nombre[i+1])<0) // > "A" a "Z" / < "Z" a "A".
              {
                   strcpy(aux,nombre[i]);
                   strcpy(nombre[i],nombre[i+1]);
@@ -39,4 +40,5 @@ main()
          printf("Nombre %d: %s\n",i,nombre[i]);
      }
 
+    system("pause");
 }
