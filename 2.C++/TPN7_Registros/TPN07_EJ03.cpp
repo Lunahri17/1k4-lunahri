e9094598074884e73efa036c5d39201221ea68f5
Lunahri17 <jeremy17games@hotmail.com>
2020-10-14 19:42:16 -0300

    Estructuras del enunciado.

--- 2.C++/TPN7_Registros/TPN07_EJ03.cpp
+++ 2.C++/TPN7_Registros/TPN07_EJ03.cpp
@@ -3,7 +3,20 @@
 #include<math.h>
 
 //Estructuras:
+struct fecha
+{
+    int dia;
+    int mes;
+    int year;
+};
 
+struct datosCliente
+{
+    char ApeNom[40];    //Apellido y nombre del cliente.
+    char dire[60];      //Domicilio particular del cliente.
+    float monVehi;      //Monto del vehiculo.
+    fecha fecVenta;     //Fecha de venta del vehiculo.
+};
 
 //Protipos de funciones:
 void end();
@@ -14,7 +27,13 @@ void end();
 
 main()
 {
-	
+	int CantidadClientes=0;
+    datosCliente Clientes;
+
+    printf("\nIngrese la cantidad de cliente a ingresar: ");
+    scanf("%d",&CantidadClientes);
+
+    
 	
 	end();
 }
