95c1aa30e49c5bd4a6cb7c20dfc1fbc67cf23114
Lunahri17 <jeremy17games@hotmail.com>
2020-10-14 19:02:10 -0300

    Ingreso de datos de clientes.

--- 2.C++/TPN7_Registros/TPN07_Ej01.cpp
+++ 2.C++/TPN7_Registros/TPN07_Ej01.cpp
@@ -8,9 +8,8 @@ struct datosCliente
     int codCli;         //Codigo del cliente.
     char ApeNom[40];    //Apellido y nombre.
     char dire[60];      //Dimicilio.
-    float monCred       //Monto máximo del crédito que puede realizar en cada compra.
+    float monCred;      //Monto máximo del crédito que puede realizar en cada compra.
 };
-
 
 //Protipos de funciones:
 void end();
@@ -21,7 +20,29 @@ void end();
 
 main()
 {
-	
+	int CantidadClientes;
+    datosCliente Clientes;
+
+    printf("\nIngrese la cantidad de cliente a ingresar: ");
+    scanf("&d",&CantidadClientes);
+
+    for (int i = 0; i < CantiadClientes; i++)
+    {
+        printf("\nIngrese el codigo del cliente %d",CantidadClientes+1);
+        scanf("%d",&Clientes.codCli);
+
+        printf("\nIngrese el apellido y nombre del cliente %d: ",CantidadClientes+1);
+        _flushall();
+        gets(Clientes.ApeNom);
+
+        printf("\nIngrese la direccion del cliente %d: ",CantidadClientes+1);
+        _flushall();
+        gets(Clientes.dire);
+
+        printf("\nIngrese el monto maximo de credito %d: ",CantidadClientes+1);
+        scanf("%d",&Clientes.monCred);
+    }
+    
 	
 	end();
 }
