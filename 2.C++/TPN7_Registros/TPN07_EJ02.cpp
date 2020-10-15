b6a18f65b42ce6658c9f0fe4ed5e7b6ca53dc78c
Lunahri17 <jeremy17games@hotmail.com>
2020-10-15 19:47:34 -0300

    Archivo para el punto

---
+++ 2.C++/TPN7_Registros/TPN07_EJ02.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+
+//Estructuras:
+
+
+//Protipos de funciones:
+void end();
+
+
+//Variables globales:
+
+
+main()
+{
+	
+	
+	end();
+}
+
+
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
