59e2e31d3a49c00ee8122482f331e27703039c11
Lunahri17 <jeremy17games@hotmail.com>
2020-10-15 21:05:42 -0300

    Estructuras declaradas.

--- 2.C++/TPN7_Registros/TPN07_EJ04.cpp
+++ 2.C++/TPN7_Registros/TPN07_EJ04.cpp
@@ -3,14 +3,32 @@
 #include<math.h>
 
 //Estructuras:
+struct Fecha
+{
+    int dia;
+    int mes
+    int year;
+};
+
+struct Notas
+{
+    int PrimerTri;
+    int SegundoTri;
+    int TercerTri;
+    int PromAnual;
+};
+
+struct Alumno
+{
+    int NroDNI;
+    char ApeNom[60];
+    Fecha Nacimiento;
+    Notas Trimestre;
+};
 
 
 //Protipos de funciones:
 void end();
-
-
-//Variables globales:
-
 
 main()
 {
