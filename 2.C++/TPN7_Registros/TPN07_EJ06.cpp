670c5575c80e79e4b88c5c0897a74eb16c8128f8
Lunahri17 <jeremy17games@hotmail.com>
2020-10-17 14:04:05 -0300

    Estructuras.

--- 2.C++/TPN7_Registros/TPN07_EJ06.cpp
+++ 2.C++/TPN7_Registros/TPN07_EJ06.cpp
@@ -5,13 +5,26 @@
 #include<string.h>
 
 //Estructuras:
+struct Fecha
+{
+    int dia;
+    int mes;
+    int year;
+};
+
+struct Clientes
+{
+    char NroExpe[40];
+    char Propietario[40];
+    float Area;
+    int TipoObra; //0: vivienda basica, 1: vivienda de Lujo, 2: Edificio, 3: predio especial.
+    Fecha Registracion[10];
+    float Monto[10];
+};
 
 
 //Protipos de funciones:
 void end();
-
-
-//Variables globales:
 
 
 main()
