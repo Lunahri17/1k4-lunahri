bcf51cfedb2bbc17c517c8dda508e4361b2655d7
Lunahri17 <jeremy17games@hotmail.com>
2020-10-20 20:17:56 -0300

    Función ingresardatos.

--- 2.C++/TPN7_Registros/TPN7_EJ07.cpp
+++ 2.C++/TPN7_Registros/TPN7_EJ07.cpp
@@ -5,6 +5,14 @@
 #include<string.h>
 
 //Estructuras:
+struct Empleados
+{
+    char ApeNom[40];
+    char Direccion[40];
+    int YearIngreso;
+    int YearProceso;
+    int Antiguedad;
+};
 
 
 //Protipos de funciones:
@@ -16,12 +24,49 @@ void end();
 
 main()
 {
-	
+	Empleados emp[20];
+    int i;
+
+    i=IngresarDatos(emp);
 	
 	end();
 }
 
+int IngresarDatos(Registro emp[20])
+{
+    int stop=1,i=0;
 
+    do
+    {
+        printf("\nIngrese el Apellido y Nombre del empleado %d: ",i+1);
+        _flushall();
+        gets(emp[i].ApeNom);
+
+        printf("\nIngrese el Domicilio del empleado %d: ",i+1);
+        _flushall();
+        gets(emp[i].Direccion);
+
+        printf("\nIngrese el anio de ingreso del empleado %d: ",i+1);
+        scanf("%d",&emp[i].YearIngreso);
+
+        printf("\nIngrese el anio de proceso del empleado %d: ",i+1);
+        scanf("%d",&emp[i].YearProceso);
+
+        i++;
+        
+        if (i!=20)
+        {
+            printf("\nDesea ingresar otro empleado? (1: Si / 0: No): ");
+            scanf("%d",&stop);
+        }
+        else
+        {
+            stop=0;
+        }       
+    } while (stop!=0);
+    
+    return i;    
+}
 
 void end()
 {
