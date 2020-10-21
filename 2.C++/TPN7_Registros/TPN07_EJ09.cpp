0b503b922bc06b2e2ff729eddf0fb12054fb2ce5
Lunahri17 <jeremy17games@hotmail.com>
2020-10-21 19:25:35 -0300

    Función CargarArticulos.

--- 2.C++/TPN7_Registros/TPN07_EJ09.cpp
+++ 2.C++/TPN7_Registros/TPN07_EJ09.cpp
@@ -5,23 +5,52 @@
 #include<string.h>
 
 //Estructuras:
+struct Articulos
+{
+    int codigo
+    char descripcion[40];
+    int stock;
+    float precio;
+};
 
 
 //Protipos de funciones:
 void end();
-
-
-//Variables globales:
-
+void CargarArticulos(int n,Articulo art[40]);
 
 main()
 {
-	
+	Articulos art[40];
+    int n;
+
+    printf("\nInrese la cantidad de articulo que ingresara: ");
+    scanf("%d",&n);
+
+    CargarArticulos(n,art);
 	
 	end();
 }
 
+void CargarArticulos(int n,Articulo art[40])
+{
+    for (int i = 0; i < n; i++)
+    {
+        system("cls");
 
+        printf("\nIngrese el codigo del articulo %d: ",i+1);
+        scanf("%d",&art[i].codigo);
+
+        printf("\nIngrese la descripcion del articulo %d: ",i+1);
+        _flushall();
+        gets(art[i].descripcion);
+        
+        printf("\nIngrese el stock del articulo %d: ",i+1);
+        scanf("%d",&art[i].stock);
+
+        printf("\nIngrese el precio del articulo %d: ",i+1);
+        scanf("%f",&art[i].precio);
+    }
+}
 
 void end()
 {
