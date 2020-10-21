17b12ada2ba3856cfc46db5aaec637deb5756810
Lunahri17 <jeremy17games@hotmail.com>
2020-10-21 16:21:03 -0300

    Estructuras.

--- 2.C++/TPN7_Registros/TPN07_EJ08.cpp
+++ 2.C++/TPN7_Registros/TPN07_EJ08.cpp
@@ -5,6 +5,22 @@
 #include<string.h>
 
 //Estructuras:
+struct Fecha
+{
+    int dia;
+    int mes;
+    int year;
+};
+
+struct Productos
+{
+    int codigo;
+    char articulo[40];
+    int cantMin;
+    Fecha compra;
+    int stock;
+    int precio;
+};
 
 
 //Protipos de funciones:
