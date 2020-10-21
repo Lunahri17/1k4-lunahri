0767cebc664f165788b899d17ce4ed3a4f2cdd6a
Lunahri17 <jeremy17games@hotmail.com>
2020-10-21 16:36:01 -0300

    Función IngresarProductos.

--- 2.C++/TPN7_Registros/TPN07_EJ08.cpp
+++ 2.C++/TPN7_Registros/TPN07_EJ08.cpp
@@ -25,19 +25,65 @@ struct Productos
 
 //Protipos de funciones:
 void end();
-
-
-//Variables globales:
-
+int IngresarProductos(Empleados pro[40]);
 
 main()
 {
-	
+	Productos pro[40];
+    int i;
+
+    i=IngresarProductos(pro);
 	
 	end();
 }
 
+int IngresarProductos(Empleados pro[40])
+{
+    int stop=1,i=0;
 
+    printf("\nIngrese al menos 10 productos")
+    
+    do
+    {
+        printf("\n\n------------------------------------------------------------------------------\n");
+
+        printf("\nIngrese el codigo del producto %d: ",i+1);
+        scanf("%d",&pro[i].codigo);
+
+        printf("\nIngrese el nombre del producto %d: ",i+1);
+        _flushall();
+        gets(pro[i].articulo);
+
+        printf("\nIngrese el stock minimo del producto %d: ",i+1);
+        scanf("%d",&pro[i].cantMin);        
+        
+        printf("\nIngrese la fecha de la ultima compra: ");
+        printf("\nDia: ");
+        scanf("%d",&pro[i].compra.dia);
+        printf("\nMes: ");
+        scanf("%d",&pro[i].compra.mes);
+        printf("\nAnio: ");
+        scanf("%d",&pro[i].compra.year);
+        
+        printf("\nIngrese el stock del producto %d: ",i+1);
+        scanf("%d",&pro[i].stock);
+
+        printf("\nIngrese el precio final del producto %d: ",i+1);
+        scanf("%d",&pro[i].precio);
+
+        i++;
+        
+        if (i>9)
+        {
+            printf("\nDesea ingresar otro empleado? (1: Si / 0: No): ");
+            scanf("%d",&stop);
+        }
+             
+    } while (stop!=0);
+    
+    return i;    
+}
+}
 
 void end()
 {
