7ba7064b6da29398e678ec765f41ba0fbc7b5ca6
Lunahri17 <jeremy17games@hotmail.com>
2020-11-03 19:30:23 -0300

    Archivo para problema 2.

---
+++ 2.C++/TPN8_Archivos/TPN8_EJ02.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include<string.h>
+
+//Estructuras:
+
+
+//Protipos de funciones:
+void end();
+
+
+//Variables globales:
+
+
+main()
+{
+	
+	
+	end();
+}
+
+
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
