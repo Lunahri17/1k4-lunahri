b56a9938e77617bf478d19438054a272e64c6918
Lunahri17 <jeremy17games@hotmail.com>
2020-11-04 16:23:33 -0300

    Archivo para ejercicio 3

---
+++ 2.C++/TPN8_Archivos/TPN08_EJ03.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+//Estructuras:
+
+
+//Protipos de funciones:
+void end();
+
+
+main()
+{
+	
+	
+	end();
+}
+
+
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
