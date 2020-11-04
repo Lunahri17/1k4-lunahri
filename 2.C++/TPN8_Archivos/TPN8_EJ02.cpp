dd786ca5bad7ac728992d7ab036fe184793c01a2
Lunahri17 <jeremy17games@hotmail.com>
2020-11-03 21:24:16 -0300

    Función aleatorios.

--- 2.C++/TPN8_Archivos/TPN8_EJ02.cpp
+++ 2.C++/TPN8_Archivos/TPN8_EJ02.cpp
@@ -3,31 +3,110 @@
 #include<math.h>
 #include<conio.h>
 #include<string.h>
+#include<ctime>
 
 //Estructuras:
 
 
 //Protipos de funciones:
-void end();
+void Aleatorios(FILE *arch);
 
-
-//Variables globales:
-
-
-main()
+main() 
 {
-	
-	
-	end();
+	FILE *arch;
+    int opcion,i=0;
+    
+    srand(time(0));
+    Aleatorios(arch);
+
+    do
+    {
+        system("cls");
+        printf("\n\tMenu:");
+        printf("\n\n1. Cantidad de valores pares e impares.");
+        printf("\n\n2. Lista.");
+        printf("\n\n3. Agragar datos.");
+        printf("\n\n4. Buscar un dato en el archivo.");
+        printf("\n\n5. Valores entre el rango [125-500] y (500-789].");
+        printf("\n\n6. Salir.");
+        printf("\n\nIngrese la opcion: ");
+        scanf("%d",&opcion);
+    
+        switch(opcion)
+        {
+            case 1:
+                
+                break;
+            
+            case 2:
+                if (i>0) {
+                    
+                }
+                else {
+                    printf("\nNo se ingresaron datos.");
+                }
+                break;
+            
+            case 3:
+                if (i>0) {
+                    
+                }
+                else {
+                    printf("\nNo se ingresaron datos.");
+                }
+                break;
+            
+            case 4:
+                if (i>0) {
+                    
+                }
+                else {
+                    printf("\nNo se ingresaron datos.");
+                }
+                break;
+            
+            case 5:
+                if (i>0) {
+                    
+                }
+                else {
+                    printf("\nNo se ingresaron datos.");
+                }
+                break;
+
+            case 6:
+                printf("\nGracias por utilizar nuestros servicios.");
+                break;
+            
+            default:
+                printf("\nIngreso una opcion incorrecta, volvera al menu.");
+                break;
+        }
+
+        printf("\n\n");
+        system("pause");
+    } 
+    while (opcion!=6);
+
+}
+
+void Aleatorios(FILE *arch)
+{
+    fopen("numeros.dat","a+b");
+    int nro_azar,cont=0;
+
+    while (!feof(arch)){
+        cont++;
+    }
+    
+    if (cont<276)
+    {
+        for (int i = 0; i < 276; i++)
+        {
+            nro_azar=125+(rand()%665);
+            fwrite(&nro_azar,sizeof(int),1,arch);
+        }   
+    }
 }
 
 
-
-void end()
-{
-	printf("\n\n");
-	system("pause");
-    printf("\n\tGracias por utilizar el programa.");
- 	printf("\n\n");
-	system("pause");
-}
