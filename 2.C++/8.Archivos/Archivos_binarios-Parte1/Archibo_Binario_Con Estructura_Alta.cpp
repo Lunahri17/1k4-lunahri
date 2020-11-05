572f8fcfd7debfbd00a304cec5e201c664387f42
Lunahri17 <jeremy17games@hotmail.com>
2020-11-05 18:14:49 -0300

    Espacios en el codigo nadamás

--- 2.C++/8.Archivos/Archivos_binarios-Parte1/Archibo_Binario_Con Estructura_Alta.cpp
+++ 2.C++/8.Archivos/Archivos_binarios-Parte1/Archibo_Binario_Con Estructura_Alta.cpp
@@ -24,14 +24,16 @@ main (void)
     printf("\n\nIngrese los datos del registro\n");
     for (i=0;i<n;i++)
     {
-        
         printf("\nCodigo   : ");
         scanf("%d",&reg.codigo);
+        
         _flushall();
         printf("Descripcion: ");
         gets(reg.descri);
+        
         printf("Precio: ");
         scanf("%f",&reg.precio);
+        
         reg.borrado=false;
         fwrite(&reg,sizeof(registro),1,arch);
     }
