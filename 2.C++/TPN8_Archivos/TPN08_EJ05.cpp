b25c62925a200d07656db42936fb88f1a07a5840
Lunahri17 <jeremy17games@hotmail.com>
2020-11-08 18:49:56 -0300

    Estructura menu principal.

--- 2.C++/TPN8_Archivos/TPN08_EJ05.cpp
+++ 2.C++/TPN8_Archivos/TPN08_EJ05.cpp
@@ -1,21 +1,52 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
 #include<conio.h>
 #include<string.h>
-
-//Estructuras:
-
+#include<windows.h>
 
 //Protipos de funciones:
 void end();
 
-
-//Variables globales:
-
-
 main()
 {
+	setlocale(LC_CTYPE,"Spanish");
+
+	FILE *arch1,*arch2,*archcomb;
+	char frase[80];
+	int opcion;
+	
+	arch1=fopen("Archivo1.txt","w");
+	arch2=fopen("Archivo2.txt","w");
+
+	do
+	{
+		system("cls");
+		printf("\n\tSistema de frases para 2 empleados.");
+		printf("\n1. Cargar frases de José Luis.");
+		printf("\n2. Cargar frases de María Emilia.");
+		printf("\n3. Terminar el día.");
+		printf("\nIngrese la opción: ");
+		scanf("%d",&opcion);
+
+		switch (opcion)
+		{
+		case 1:
+			
+			break;
+
+		case 2:
+			
+			break;
+
+		case 3:
+			
+			break;
+		
+		default:
+			printf("\nIngresó una opción incorrecta, vuelva a intentarlo.");
+			break;
+		}
+	} while (opcion!=3);
 	
 	
 	end();
