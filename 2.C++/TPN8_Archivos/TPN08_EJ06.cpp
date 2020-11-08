79f26c14d9b758240a01264e5b4d2c8ea4c67551
Lunahri17 <jeremy17games@hotmail.com>
2020-11-08 20:42:18 -0300

    Archivo para punto 6

---
+++ 2.C++/TPN8_Archivos/TPN08_EJ06.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<string.h>
+
+//Protipos de funciones:
+void end();
+
+main()
+{
+	
+	
+	end();
+}
+
+
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
