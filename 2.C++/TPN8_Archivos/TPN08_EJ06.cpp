502320112cb95c3308dd50b4ecfd4479a1724aa1
Lunahri17 <jeremy17games@hotmail.com>
2020-11-10 18:30:11 -0300

    Función cargar_estudiantes

--- 2.C++/TPN8_Archivos/TPN08_EJ06.cpp
+++ 2.C++/TPN8_Archivos/TPN08_EJ06.cpp
@@ -8,11 +8,53 @@ void end();
 
 main()
 {
+	FILE *arch;
 	
+	cargar_estudiantes(arch);
 	
 	end();
 }
 
+void cargar_estudiantes(arch)
+{
+	int n;
+	char estudiante[150],aux[40];
+
+	arch=fopen("Estudiantes.txt","w");
+
+	printf("\nIngrese la cantidad de estudiantes: ");
+	scanf("%d",&n);
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("\nIngrese el Apellido del estudiante %d: ",i+1);
+		_flushall();
+		gets(estudiante);
+		strcat(estudiante,';');
+
+		printf("\nIngrese el Nombre del estudiante %d: ",i+1);
+		_flushall();
+		gets(aux);
+		strcat(estudiante,aux);
+		strcat(estudiante,';');
+
+		printf("\nIngrese la Carrera del estudiante %d: ",i+1);
+		_flushall();
+		gets(aux);
+		strcat(estudiante,aux);
+		strcat(estudiante,';');
+		
+		printf("\nIngrese el promedio del estudiante %d: ",i+1);
+		_flushall();
+		gets(aux);
+		strcat(estudiante,aux);
+		strcat(estudiante,';');
+
+		fprintf(arch,strcat(estudiante,"\n"));
+	}
+
+	fclose(arch);
+}
 
 
 void end()
