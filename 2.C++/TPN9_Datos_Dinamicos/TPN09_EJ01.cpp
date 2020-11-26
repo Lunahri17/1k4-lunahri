78bb626cfd1d678461896543c0bb89aa0a2a4f50
Lunahri17 <jeremy17games@hotmail.com>
2020-11-26 20:08:16 -0300

    Función borrar y otras más

--- 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ01.cpp
+++ 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ01.cpp
@@ -10,6 +10,8 @@ struct nodo{
 //Protipos de funciones:
 void end();
 void insertar_lista(nodo *&Lista,float valor);
+void mostrar_lista(nodo *Lista);
+void borrar_nodo(nodo *&Lista,int valor);
 
 main()
 {
@@ -28,17 +30,21 @@ main()
         insertar_lista(Lista,valor);
     }
     
+    borrar_nodo(Lista,valor);
+
+    mostrar_lista(Lista);
 	
 	end();
 }
 
-
 void insertar_lista(nodo *&Lista,float valor)
 {
-	nodo *aux = Lista,*aux2, *nuevo_nodo = new(nodo); //pide un nuevo nodo.
+	nodo *nuevo_nodo = new(nodo); //pide un nuevo nodo.
 	
     nuevo_nodo->nro = valor;
 	
+    nodo *aux = Lista,*aux2;
+
     while ((aux != NULL) and (aux->nro < valor))
     {
         aux2 = aux;
@@ -57,6 +63,36 @@ void insertar_lista(nodo *&Lista,float valor)
     nuevo_nodo->sgte = aux;
 }
 
+void borrar_nodo(nodo *&Lista,int valor)
+{
+    if (Lista!=NULL)
+    {
+        nodo *aux_borrar=Lista;
+        nodo *anterior=NULL;
+
+        while((aux_borrar!=NULL) and (aux_borrar->nro!=valor))
+        {	
+            anterior=aux_borrar;
+            aux_borrar=aux_borrar->sgte;
+        }
+    }
+}
+
+
+void mostrar_lista(nodo *Lista)
+{
+    nodo *actual = new(nodo);
+    actual=Lista;
+
+    int i = 0;
+	printf("\nListado de los elementos:\n");
+	while(actual != NULL)
+	{	
+		printf("Elemento %d: %.2f \n", i+1, actual->nro);
+		actual = actual->sgte;
+		i++;
+	}	
+}
 
 void end()
 {
