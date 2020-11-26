7828902917015727672ee3cbf2b70de7b3fcafe3
Lunahri17 <jeremy17games@hotmail.com>
2020-11-26 15:54:48 -0300

    Archivo para punto 1 tpn9

---
+++ 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ01.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<conio.h>
+#include<string.h>
+
+//Estructuras:
+
+
+//Protipos de funciones:
+void end();
+
+
+main()
+{
+	
+	
+	end();
+}
+
+
+
+void end()
+{
+	printf("\n\n");
+	system("pause");
+    printf("\n\tGracias por utilizar el programa.");
+ 	printf("\n\n");
+	system("pause");
+}
