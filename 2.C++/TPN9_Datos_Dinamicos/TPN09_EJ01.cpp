47d8505f30837e9f6ca029d454a0eec7be9dcdce
Lunahri17 <jeremy17games@hotmail.com>
2020-11-26 16:51:01 -0300

    no entiendo una mierda

--- 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ01.cpp
+++ 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ01.cpp
@@ -1,23 +1,59 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
-#include<conio.h>
-#include<string.h>
 
 //Estructuras:
-
+struct nodo{
+	int nro;
+	struct nodo *sgte;
+};
 
 //Protipos de funciones:
 void end();
-
+void insertarNodo(nodo *&finLista, int valor);
 
 main()
 {
-	
+	nodo *finLista = NULL;
+    int n;
+    float valor;
+
+    printf("\nIngrese el tamnio de la lista: ");
+    scanf("%d",&n);
+
+    //Carga la lista:
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nIngrese el valor real %d: ",i+1);
+        scanf("%f",&valor);
+        insertarNodo(finLista, valor);
+    }
+    
 	
 	end();
 }
 
+/*-------------------- Insertar siguiente Elemento-------------------------*/
+void insertarNodo(nodo *&finLista, int valor)
+{
+	nodo *t, *q = new(nodo); //pide un nuevo nodo.
+	
+    q->nro  = valor;
+	q->sgte = NULL;
+	
+    if(finLista==NULL)
+	{
+		finLista = q;
+	} 
+    else
+    {		
+		t = finLista;
+		while(t->sgte!=NULL)
+		{		
+			t = t->sgte;
+		}	
+		t->sgte = q;
+	}
+}
 
 
 void end()
