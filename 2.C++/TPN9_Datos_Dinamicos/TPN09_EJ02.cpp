a25802eca45f60e16c20192cdb7878c211a73975
Lunahri17 <jeremy17games@hotmail.com>
2020-11-27 16:08:51 -0300

    nodo modificado.

--- 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ02.cpp
+++ 2.C++/TPN9_Datos_Dinamicos/TPN09_EJ02.cpp
@@ -1,21 +1,124 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<conio.h>
+#include<string.h>
 
 //Estructuras:
-
+struct nodo{
+	int leg;
+    char apenom[50];
+    int edad;
+    nodo *sgte;
+};
 
 //Protipos de funciones:
 void end();
-
+void insertar_lista(nodo *&Lista,float valor);
+void mostrar_lista(nodo *Lista);
+void borrar_nodo(nodo *&Lista,int valor);
 
 main()
 {
-	
+	nodo *Lista = NULL;
+    int n,legajo,anios;
+    char apeynom[50];
+
+    printf("\nIngrese la cantidad de empleados: ");
+    scanf("%d",&n);
+
+    //Carga la lista:
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nIngrese el legajo del empleado %d: ",i+1);
+        scanf("%d",&legajo);
+        printf("\nIngrese el legajo del empleado %d: ",i+1);
+        scanf("%d",&legajo);
+        printf("\nIngrese el legajo del empleado %d: ",i+1);
+        scanf("%d",&legajo);
+        insertar_lista(Lista,valor);
+    }
+    
+    printf("\nIngrese el valor que desea eliminar: ");
+    scanf("%f",&valor);
+    borrar_nodo(Lista,valor);
+
+    mostrar_lista(Lista);
 	
 	end();
 }
 
+void insertar_lista(nodo *&Lista,float valor)
+{
+	nodo *nuevo_nodo = new(nodo); //pide un nuevo nodo.
+	
+    nuevo_nodo->nro = valor;
+	
+    nodo *aux = Lista,*aux2;
 
+    while ((aux != NULL) and (aux->nro < valor))
+    {
+        aux2 = aux;
+        aux = aux->sgte;
+    }
+    
+    if (Lista == aux)
+    {
+        Lista = nuevo_nodo;
+    }
+    else
+    {
+        aux2->sgte = nuevo_nodo;
+    }
+    
+    nuevo_nodo->sgte = aux;
+}
+
+void borrar_nodo(nodo *&Lista,int valor)
+{
+    if (Lista!=NULL)
+    {
+        nodo *aux_borrar=Lista;
+        nodo *anterior=NULL;
+
+        while((aux_borrar!=NULL) and (aux_borrar->nro!=valor))
+        {	
+            anterior=aux_borrar;
+            aux_borrar=aux_borrar->sgte;
+        }
+
+        if (aux_borrar==NULL)
+        {
+            printf("\nEl elemento buscado no se encontro.");
+        }
+        
+        if (anterior==NULL)
+        {
+            Lista=Lista->sgte;
+            delete aux_borrar;
+        }
+        else
+        {
+            anterior->sgte=aux_borrar->sgte;
+            delete aux_borrar;
+        }        
+    }
+}
+
+
+void mostrar_lista(nodo *Lista)
+{
+    nodo *actual = new(nodo);
+    actual=Lista;
+
+    int i = 0;
+	printf("\nListado de los elementos:\n");
+	while(actual != NULL)
+	{	
+		printf("Elemento %d: %.2f \n", i+1, actual->nro);
+		actual = actual->sgte;
+		i++;
+	}	
+}
 
 void end()
 {
